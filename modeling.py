acc66faa120de542a0a2d4dc14fb565512377b18
RA Stephen <alick1580@gmail.com>
2019-11-16 20:25:25 -0500

    commit

--- modeling.py
+++ modeling.py
@@ -13,16 +13,24 @@ from sklearn.model_selection import RandomizedSearchCV
 
 # DATA DUMMIFICATION
 all_data_dumify = pd.get_dummies(data=all_data, drop_first=True)
-
+all_data_dumify.shape
 # SPLITTING DATA
 xtrain = all_data_dumify[:ntrain]
 xtest = all_data_dumify[ntrain:]
 
 xtrain_no_dummify = all_data[:ntrain]
 xtest_no_dummify = all_data[ntrain:]
+# xtrain_no_dummify["BldgType"] = lb_make.fit_transform(xtrain_no_dummify["BldgType"])
+xtrain_no_dummify.dtypes
 
+char_cols = xtrain_no_dummify.dtypes.pipe(lambda x: x[x == 'object']).index
 
-all_data
+# Encode All Categorical Data For Decision TreeRegression
+for c in char_cols:
+    xtrain_no_dummify[c] = pd.factorize(xtrain_no_dummify[c])[0]
+xtrain_no_dummify.dtypes
+# xtrain_no_dummify["Electrical"]
+
 df_train = all_data_dumify[:ntrain]
 df_test = all_data_dumify[ntest:]
 
@@ -34,72 +42,75 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 
 # *****************************************************************************************************************************************************************************
 # *****************************************************************************************************************************************************************************
-#random forest
+# #random forest
 
-n_estimators = range(1,100)
-#[int(x) for x in np.linspace(start = 1, stop = 100, num = 1)]
-# Number of features to consider at every split
-max_features = ['auto', 'sqrt']
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
-#cretion
-criterion=['mse']
-# Create the random grid
-random_grid = {'n_estimators': n_estimators,
-               'max_features': max_features,
-               'max_depth': max_depth,
+# n_estimators = range(1,100)
+# #[int(x) for x in np.linspace(start = 1, stop = 100, num = 1)]
+# # Number of features to consider at every split
+# max_features = ['auto', 'sqrt']
+# # Maximum number of levels in tree
+# max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
+# max_depth.append(None)
+# # Minimum number of samples required to split a node
+# min_samples_split = [2, 5, 10]
+# # Minimum number of samples required at each leaf node
+# min_samples_leaf = [1, 2, 4]
+# # Method of selecting samples for training each tree
+# bootstrap = [True, False]
+# #cretion
+# criterion=['mse']
+# # Create the random grid
+# random_grid = {'n_estimators': n_estimators,
+#                'max_features': max_features,
+#                'max_depth': max_depth,
                
-               'min_samples_split': min_samples_split,
-               'min_samples_leaf': min_samples_leaf,
-               'bootstrap': bootstrap
-               }
-pprint(random_grid)
+#                'min_samples_split': min_samples_split,
+#                'min_samples_leaf': min_samples_leaf,
+#                'bootstrap': bootstrap
+#                }
+# pprint(random_grid)
 
 
-# Use the random grid to search for best hyperparameters
-# First create the base model to tune
-rf = RandomForestRegressor()
-# Random search of parameters, using 3 fold cross validation, 
-# search across 100 different combinations, and use all available cores
-rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
-# Fit the random search model
-rf_random.fit(xtrain, y_train_final)
+# # Use the random grid to search for best hyperparameters
+# # First create the base model to tune
+# rf = RandomForestRegressor()
+# # Random search of parameters, using 3 fold cross validation, 
+# # search across 100 different combinations, and use all available cores
+# rf_random = RandomizedSearchCV(estimator = rf, param_distributions = random_grid, n_iter = 100, cv = 3, verbose=2, random_state=42, n_jobs = -1)
+# # Fit the random search model
+# rf_random.fit(xtrain, y_train_final)
 
-#bset params
-rf_random.best_params_
+# #bset params
+# rf_random.best_params_
 
+X_no_dummify = xtrain_no_dummify.loc[:, ~xtrain_no_dummify.columns.isin(['SalePrice'])]
+X_train_no_dummify, X_test_no_dummify, y_train_no_dummify, y_test_no_dummify = train_test_split(X_no_dummify, y, test_size=0.2, random_state=42)
 
-rf = RandomForestRegressor(n_estimators = 94,min_samples_split=5,min_samples_leaf=1,max_features='sqrt',
-                           max_depth=70,bootstrap=False)
+# xtrain_no_dummify
+rf = RandomForestRegressor(n_estimators = 94,min_samples_split=5,min_samples_leaf=1,max_features='sqrt',max_depth=70,bootstrap=False)
 
 #fit with best params of the train data
-rf.fit(X_train, y_train)
+rf.fit(X_train_no_dummify, y_train_no_dummify)
 
 
 #score with train data
-rf.score(X_train, y_train)
-
+rf.score(X_train_no_dummify, y_train_no_dummify)
 #predict
-y_predicted=rf.predict(X_test)
-
+y_predicted=rf.predict(X_test_no_dummify)
 # fit test data with npredicted
-rf.fit(X_test,y_predicted)
-
+rf.fit(X_test_no_dummify,y_predicted)
 #check score
-rf.score(X_test,y_predicted)
-# #checking error
-# train_error = (1 - train_score)
-# test_error = (1 - train_score)
-# print("The training error is: %.5f" %train_error)
-# print("The test     error is: %.5f" %test_error)
+rf.score(X_test_no_dummify,y_predicted)
 
+#select the most impotant features in random forest
+pd.set_option('display.max_rows', None)
+X_train_no_dummify.shape
+important_features = pd.Series(data=rf.feature_importances_,index=X_train_no_dummify.columns)
+important_features.sort_values(ascending=False,inplace=True)
+important_features
+
+#plot graph of most import feature
+important_features.plot(kind = 'bar')
 
 
 
@@ -256,7 +267,6 @@ from subprocess import check_output
 from sklearn.model_selection import GridSearchCV
 
 
-
 print('Mean Absolute Error:', metrics.mean_absolute_error(y_pred_final, y_pred_rndfrst))  
 print('Mean Squared Error:', metrics.mean_squared_error(y_pred_final, y_pred_rndfrst))  
 print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_pred_final, y_pred_rndfrst)))
@@ -277,6 +287,8 @@ submit['id'] = test_ID
 submit['SalePrice'] = pd.DataFrame(y_pred_final)
 # ----------------------------- Create File to Submit --------------------------------
 # submit.to_csv('SalePrice_N_submission.csv', index = False)
-submit.to_csv('./submission/SalePrice_N_submission7.csv', index = False)
+# submit.to_csv('./submission/SalePrice_N_submission9.csv', index = False)
 
 submit.head()
+
+important_features.plot(kind = 'bar')
