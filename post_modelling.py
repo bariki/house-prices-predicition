e956b47d75d71d307d0f16fcd7e69aec9214fc2c
katisho <georgelucas@georges-mbp.home>
2019-11-17 21:19:17 -0500

    power point another modification

--- post_modelling.py
+++ post_modelling.py
@@ -14,7 +14,13 @@ df1_head = df.head(30)
 # df
 
 # Histogram
-df.plot(kind='bar',figsize=(10,8))
+df1_head.plot(kind='bar',figsize=(10,8))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+# # Scatter Plot
+df1_head.plot(kind='line',figsize=(10,8))
 plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
 plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
 plt.show()
